c9843622c604b23fb301dc61c185996e8705c55a
Romannoss <giovanni.lcb@gmail.com>
2021-10-14 20:36:07 -0300

    update 14/10/21
    update 14/10/21 - atrasado

---
+++ HTML - CSS/prd passagem/script.js
@@ -0,0 +1,50 @@
+const info = [
+
+
+
+]
+
+const infotable = {
+all: info,
+
+
+    add(infotable){
+        infotable.all.push(infotable)
+
+    },
+
+    remove(){
+
+    }
+
+
+}
+
+const form = {
+
+    submit(event) {
+       event.preventDefault()
+
+    }
+
+
+}
+
+/*
+const app = {
+
+    init() {
+
+
+
+
+    },
+
+
+    reload() {
+
+
+    }
+}
+
+*/
