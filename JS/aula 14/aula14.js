6041958d49788eff8d944438cb926de1cf68b7ca
Giovanni Bisio <43917212+Romannoss@users.noreply.github.com>
2023-03-22 01:05:44 -0300

    delete JS path
    delete JS path

--- JS/aula 14/aula14.js
+++
@@ -1,4 +0,0 @@
-for( var cont = 1 ; cont <= 6 ; cont++ ){
-
-    console.log(cont)
-}
--- JS/aula 15/vetor.js
+++
@@ -1,7 +0,0 @@
-var num = [5, 4, 9, 3, 6,0]
-
-
-num.sort()
-// num.push(2)
-console.log(num)
-console.log(`primeiro vetor eh ${num[0]}`)
