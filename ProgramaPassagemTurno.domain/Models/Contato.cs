219a05f58996dc918a3e37cc7870e2804847133e
Giovanni <giovanni.lcb@gmail.com>
2023-03-22 00:56:00 -0300

    update 22/03/23
    criação de pasta domain

---
+++ ProgramaPassagemTurno.domain/Models/Contato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProgramaPassagemTurno.Domain.Models
+{
+    public class Contato
+    {
+
+        public int Telefone { get; set; }
+
+        public string Equipe { get; set; }
+
+        public string Turno { get; set; }
+
+        public string Usuario { get; set; }
+
+
+    }
+}
