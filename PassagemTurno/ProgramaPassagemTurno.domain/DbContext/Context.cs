fb54ca46d33ceeb4840821c285eb0681c1ab9dd3
Daniel Nicodemos <dani_nicodemos@live.com>
2023-03-24 21:51:24 -0300

    arrumando a merda que esse maldito fez

---
+++ PassagemTurno/ProgramaPassagemTurno.domain/DbContext/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramaPassagemTurno.Domain.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<CadastrarPassagem> CadastrarPassagem { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-FOFBEVP\SQLEXPRESS;Initial Catalog=PassagemDeTurno;Integrated Security=True");
+        }
+    }
+}
--- ProgramaPassagemTurno.domain/ViewModel/Error/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProgramaPassagemTurno.Domain.ViewModel.Error
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
